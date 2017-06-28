11dcacd3ce5ec7a94a38af2f949aca03d73805d1
Silas Ribas Martins <silasrm@gmail.com>
2017-06-28 13:34:00 -0300

    Change auth config to per server

--- Extended/Cache/Backend/Redis.php
+++ Extended/Cache/Backend/Redis.php
@@ -6,9 +6,9 @@
   *
   * New BSD License
   *
-  * Redistribution and use in source and binary forms, with or without 
+  * Redistribution and use in source and binary forms, with or without
   * modification, are permitted provided that the following conditions are met:
-  * 
+  *
   *  * Redistributions of source code must retain the above copyright notice,
   *    this list of conditions and the following disclaimer.
   *
@@ -21,15 +21,15 @@
   *    software without specific prior written permission.
   *
   * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
-  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
-  * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE 
-  * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
-  * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
-  * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
-  * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
-  * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
-  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+  * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+  * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+  * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+  * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+  * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+  * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
   * POSSIBILITY OF SUCH DAMAGE.
   **/
 
@@ -117,14 +117,14 @@ class Extended_Cache_Backend_Redis extends Zend_Cache_Backend implements Zend_Ca
                 $result = $this->_redis->connect($server['host'], $server['port']);
             }
 
+            if ($this->_redis && $server['auth']) {
+                $this->_redis->auth($server['password']);
+            }
+
             if ($result)
                 $this->_redis->select($server['dbindex']);
             else
                 $this->_redis = null;
-        }
-
-        if ($this->_redis && $this->_options['auth']) {
-            $this->_redis->auth($this->_options['password']);
         }
     }
 
--- README.markdown
+++ README.markdown
@@ -18,11 +18,11 @@ $redisCache = Zend_Cache::factory(
                 'port' => 6379,
                 'dbindex' => 1,
                 // 'persistent' => false, // not a persistent connection
+                // 'auth' => true, // enable authentication
+                // 'password' => 'mypwd000000', // password to authenticate on redis server
             ),
         ),
         // 'key_prefix' => 'my_app', // if desire to add a prefix to all cache keys
-        // 'auth' => true, // enable authentication
-        // 'password' => 'mypwd000000', // password to authenticate on redis server
     ))
 );
 </pre>
